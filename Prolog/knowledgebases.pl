38d9ec99a7c15528bea4e986b6f498efa1a0690c
NotSamarth <samarthvaishnav33@gmail.com>
2024-03-08 17:56:57 +0530

    prolog code

---
+++ Prolog/knowledgebases.pl
@@ -0,0 +1,16 @@
+loves(vincent, mia).
+loves(marcellus, mia).
+loves(pumpkin, honey_bunny).
+loves(honey_bunny, pumpkin).
+
+jealous(X, Y) :-
+    loves(X, Z),
+    loves(Y, Z).
+
+
+/** <examples>
+
+?- loves(X, mia).
+?- jealous(X, Y).
+
+*/
